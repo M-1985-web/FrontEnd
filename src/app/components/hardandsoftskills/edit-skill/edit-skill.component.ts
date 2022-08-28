aba07a13c5016e01b9cbc94be77b395c7f91f1fa
M-1985-web <martinmarcelomaldonado1985@gmail.com>
2022-08-28 18:35:27 -0300

    corregi skill editar

--- src/app/components/hardandsoftskills/edit-skill/edit-skill.component.ts
+++ src/app/components/hardandsoftskills/edit-skill/edit-skill.component.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Skill } from 'src/app/model/skill';
+import { SkillService } from 'src/app/service/skill.service';
+import { TokenService } from 'src/app/service/token.service';
 
 @Component({
   selector: 'app-edit-skill',
   templateUrl: './edit-skill.component.html',
-  styleUrls: ['./edit-skill.component.css']
+  styleUrls: ['./edit-skill.component.css'],
 })
 export class EditSkillComponent implements OnInit {
+  skill: Skill = null;
+  //skillsoft: SoftSkill = null;
+  isLogged = false;
 
-  constructor() { }
+  constructor(
+    private activatedRouter: ActivatedRoute,
+    private router: Router,
+    private skills: SkillService,
+    private tokenService: TokenService
+  ) {}
 
   ngOnInit(): void {
+    if (this.tokenService.getToken()) {
+      this.isLogged = true;
+    } else {
+      this.isLogged = false;
+    }
+
+    const id = this.activatedRouter.snapshot.params['id'];
+    this.skills.detail(id).subscribe(
+      (data) => {
+        this.skill = data;
+      },
+      (err) => {
+        alert('Error al modicar skills');
+        this.router.navigate(['']);
+      }
+    );
   }
 
+  updatehard(): void {
+    const id = this.activatedRouter.snapshot.params['id'];
+    this.skills.update(id, this.skill).subscribe(
+      (data) => {
+        alert('Skill actualizada.');
+        this.router.navigate(['']);
+      },
+      (err) => {
+        alert('No se pudo actualizar.');
+      }
+    );
+  }
 }
