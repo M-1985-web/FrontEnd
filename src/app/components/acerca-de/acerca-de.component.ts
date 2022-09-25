3fa80b548d8d4d9bfe7300f2b90a5df2b0e8e8ed
M-1985-web <martinmarcelomaldonado1985@gmail.com>
2022-09-25 15:57:28 -0300

    componente acerca de

--- src/app/components/acerca-de/acerca-de.component.ts
+++ src/app/components/acerca-de/acerca-de.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Persona } from 'src/app/model/persona';
-//import { TokenService } from 'src/app/service/token.service';
+import { TokenService } from 'src/app/service/token.service';
 import { PersonaService } from '../../service/persona.service';
 
 @Component({
@@ -11,29 +11,29 @@ import { PersonaService } from '../../service/persona.service';
 export class AcercaDeComponent implements OnInit {
   //aca llamaria a nombre:, apellido:,img de persona, lo inicializamos
   //model
+  //persona: Persona[] = [];
 
   persona: Persona = new Persona('', '', '', '', '');
 
   //el constructor llama al servicio
   constructor(
     private personaService: PersonaService,
-    //private tokenService: TokenService
+    private tokenService: TokenService
   ) {}
 
-  //isLogged = false;
+  isLogged = false;
 
   ngOnInit(): void {
     //los metodos van entre parentesis
     //subcribe: conecta el observable con eventos observables
     //this.personaService.getPersona().subscribe(data =>{this.persona = data})
-    this.personaService.getPersona().subscribe(data=>{this.persona = data})
+    this.personaService.getPersona().subscribe((data) => {
+      this.persona = data})
 
-
-
-      //if (this.tokenService.getToken()) {
-       //this.isLogged = true;
-      //} else {
-        //this.isLogged = false;
-     //}
+    if (this.tokenService.getToken()) {
+      this.isLogged = true;
+    } else {
+      this.isLogged = false;
+    }
   }
 }
